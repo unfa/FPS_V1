ad1d90befcf3ca003ce0a5a5e56a02fc9c5d0428
unfa <unfa00@gmail.com>
2018-06-21 00:18:47 +0200

    Fixed memory leak with bullet hit particles

--- Assets/Weapons/Effects/Timer.gd
+++ Assets/Weapons/Effects/Timer.gd
@@ -1,19 +1,15 @@
-extends Spatial
+extends Timer
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	print("Pow!")
-	$Particles.emitting = true
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-
-
-func _on_Timer_timeout():
-	print("Deleted!")
-	queue_free()
