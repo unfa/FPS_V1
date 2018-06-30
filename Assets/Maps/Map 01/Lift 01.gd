9d37659a73e59106380742cc67b2231f15efb75a
unfa <unfa00@gmail.com>
2018-06-30 14:48:00 +0200

    Almost fixed lifts.

--- Assets/Maps/Map 01/Lift 01.gd
+++ Assets/Maps/Map 01/Lift 01.gd
@@ -18,13 +18,21 @@ func _process(delta):
 
 func _on_Trigger_body_entered(body):
 	
-	# check for collision with a player
+	# check for collision with a player entering lift
 	if body.name == "Player":
-		ap.playback_speed = 1
-		ap.play("Action")
+		if not ap.is_playing():
+			ap.playback_speed = 1 # make sure we're not playing backwards still
+			ap.play("Action") # play the action
+		else:
+			ap.playback_speed = - ap.playback_speed
 
 
 func _on_Trigger_body_exited(body):
+	# check for collision with a player exiting lift
+	
 	if body.name == "Player":
-		ap.playback_speed = -1
+		if ap.is_playing(): # if the player exited before the Acton was finished
+			ap.playback_speed = -ap.playback_speed # reverse the direction
+		else:
+			ap.play_backwards() # otherwise - start playback backwards
 		
